0cd51da10da2f3d8ce9f503c582cb71697e6453c
Bee <79103716+beesmalley@users.noreply.github.com>
2023-03-27 10:20:26 -0400

    update

---
+++ login-submit.php
@@ -0,0 +1,35 @@
+<?php 
+    $file =file("data.txt");
+    $user='';
+    $pass='';
+    $found=false;
+    for($i=0;$i<count($file);$i++){
+        $personinfo=explode(",",$file[$i]);
+        if($personinfo[0]==$_POST["uname"]){
+            $user=$file[$i];
+            if($personinfo[1]==$_POST["pass"]){
+                $found=true;
+                break;
+            }
+        }
+    }
+    if(!$found){
+        echo "Incorrect username or password.";
+    }else{
+        echo "Successfully logged in.";
+        }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" text="text/css" href="nerdieluv.css">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
