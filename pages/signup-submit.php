2d83fba8c11af7bec9feb74d42c44ccdc2ffb5c4
Bee <79103716+beesmalley@users.noreply.github.com>
2023-03-27 12:42:45 -0400

    path update

--- pages/signup-submit.php
+++ pages/signup-submit.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style/main.css">
+    <link rel="stylesheet" type="text/css" href="theme/main.css">
     <title>Successful Registration!</title>
 </head>
 <body>
