52a698376e1a4dcae1aae16b9dd560e5fde48019
Bee <79103716+beesmalley@users.noreply.github.com>
2023-03-27 15:26:14 -0400

    Update check.php

--- check.php
+++ check.php
@@ -8,16 +8,28 @@ if($_REQUEST["first"]){
     if($_POST["first"]==$stage1){
         $_SESSION["score"]++;
     }
+    echo "<div id=\"box\"> 
+    <p><a href=\"stage2.html\">Move on to stage 2.</a></p>
+    <p>Current score: ".$_SESSION["score"]."</p>
+    </div>";
 }
 if($_REQUEST["second"]){
     if($_POST["second"]==$stage2){
         $_SESSION["score"]++;
     }
+    echo "<div id=\"box\"> 
+    <p><a href=\"stage3.html\">Move on to stage 3.</a></p>
+    <p>Current score: ".$_SESSION["score"]."</p>
+    </div>";
 }
 if($_REQUEST["third"]){
     if($_POST["third"]==$stage3){
         $_SESSION["score"]++;
     }
+    echo "<div id=\"box\"> 
+    <p><a href=\"stage4.html\">Move on to stage 4.</a></p>
+    <p>Current score: ".$_SESSION["score"]."</p>
+    </div>";
 }
 
 ?>
