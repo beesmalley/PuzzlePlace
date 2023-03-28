ca593d10a10a1827244bd92e91a5e3ee69e0eb87
Bee <79103716+beesmalley@users.noreply.github.com>
2023-03-28 13:21:35 -0400

    Update check.php

--- check.php
+++ check.php
@@ -54,7 +54,7 @@ if(isset($_POST["fifth"])&&$_REQUEST["fifth"]){
 	<p><a href=\"leaderboard.php\">View leaderboard</a></p>
     </div>";
 
-    $file=file("score.txt");
+    $file="score.txt";
     $newdata="\n".$_SESSION["name"].",".$_SESSION["score"];
     file_put_contents($file,$newdata,FILE_APPEND);
 
